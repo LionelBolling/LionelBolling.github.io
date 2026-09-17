90c0c57aad6e97429a16a454c02cb56b18c824c2
LionelBolling <lebolling@aggies.ncat.edu>
2026-09-17 19:12:13 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,11 +1,11 @@
 let calculations = [];
 let validResults = [];
-let continueCalculating = true;
+let shouldContinue = true;
 
 
 // This will loop until the user clicks "Cancel"
 // This code ask the user for two numbers and an operator and then performs the calculation and stores the result.
-while (continueCalculating) {
+while (shouldContinue) {
     let xInput = prompt("Please Enter X:");
 
     if (xInput === null) {
@@ -89,60 +89,12 @@ while (continueCalculating) {
         validResults.push(result);
     }
 
-    continueCalculating = confirm(
+    shouldContinue = confirm(
         "Do you want to perform another calculation?"
     );
 }
 
 // This grabs all the calculations and displays them in a table format.
-document.write("<h2>Calculation Results</h2>");
-document.write("<table>");
-
-document.write("<tr>");
-document.write("<th>x</th>");
-document.write("<th>op</th>");
-document.write("<th>y</th>");
-document.write("<th>result</th>");
-document.write("</tr>");
-
-if (calculations.length === 0) {
-    document.write(
-        "<tr><td colspan='4'>No calculations were entered.</td></tr>"
-    );
-} else {
-    for (let i = 0; i < calculations.length; i++) {
-        document.write("<tr>");
-
-        document.write(
-            "<td>" + calculations[i].x + "</td>"
-        );
-
-        document.write(
-            "<td>" + calculations[i].operator + "</td>"
-        );
-
-        document.write(
-            "<td>" + calculations[i].y + "</td>"
-        );
-
-        if (calculations[i].valid) {
-            document.write(
-                "<td>" + calculations[i].result + "</td>"
-            );
-        } else {
-            document.write(
-                "<td class='error'>" +
-                calculations[i].result +
-                "</td>"
-            );
-        }
-
-        document.write("</tr>");
-    }
-}
-
-document.write("</table>");
-
 document.write("<h2>Summary of Valid Results</h2>");
 document.write("<table>");
 
@@ -153,29 +105,37 @@ document.write("<th>Average</th>");
 document.write("<th>Total</th>");
 document.write("</tr>");
 
+if (validResults.length === 0) {
+    document.write("<tr>");
+    document.write(
+        "<td colspan='4'>There are no valid results to summarize.</td>"
+    );
+    document.write("</tr>");
+} else {
+    let minimum = validResults[0];
+    let maximum = validResults[0];
+    let total = 0;
 
-let minimum = validResults[0];
-let maximum = validResults[0];
-let total = 0;
+    for (let i = 0; i < validResults.length; i++) {
+        if (validResults[i] < minimum) {
+            minimum = validResults[i];
+        }
 
-for (let i = 0; i < validResults.length; i++) {
-    if (validResults[i] < minimum) {
-        minimum = validResults[i];
+        if (validResults[i] > maximum) {
+            maximum = validResults[i];
+        }
+
+        total = total + validResults[i];
     }
 
-    if (validResults[i] > maximum) {
-        maximum = validResults[i];
-    }
+    let average = total / validResults.length;
 
-    total = total + validResults[i];
+    document.write("<tr>");
+    document.write("<td>" + minimum + "</td>");
+    document.write("<td>" + maximum + "</td>");
+    document.write("<td>" + average + "</td>");
+    document.write("<td>" + total + "</td>");
+    document.write("</tr>");
 }
 
-let average = total / validResults.length;
-
-document.write("<tr>");
-document.write("<th>Min</th>");
-document.write("<th>Max</th>");
-document.write("<th>Average</th>");
-document.write("<th>Total</th>");
-document.write("</tr>");
 document.write("</table>");
